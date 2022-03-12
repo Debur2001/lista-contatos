662daa8852662dabda670ef54a978831b191e5df
Debur <vini.bernert@yahoo.com.br>
2022-03-12 14:27:55 -0300

    Implementa options menus

--- app/src/main/java/com/example/listacontatos/view/MainActivity.kt
+++ app/src/main/java/com/example/listacontatos/view/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.listacontatos.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listacontatos.R
@@ -42,5 +46,35 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item do menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item do menu 2")
+                true
+            }
+            R.id.item_menu_3 -> {
+                showToast("Exibindo item do menu 3")
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
