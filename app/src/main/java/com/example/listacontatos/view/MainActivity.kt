b6ef6be864697dee1f601b3eb839d17df78d729e
Debur <vini.bernert@yahoo.com.br>
2022-03-12 20:18:02 -0300

    Testa o funcionamento do Shared Preferences

--- app/src/main/java/com/example/listacontatos/view/MainActivity.kt
+++ app/src/main/java/com/example/listacontatos/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.listacontatos.view
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,6 +12,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +21,8 @@ import com.example.listacontatos.model.Contact
 import com.example.listacontatos.view.DetailActivity.Companion.EXTRA_CONTACT
 import com.example.listacontatos.viewmodel.ClickItemContactListener
 import com.example.listacontatos.viewmodel.ContactAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private val rvList: RecyclerView by lazy {
@@ -30,8 +35,31 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         setContentView(R.layout.drawer_menu)
 
         initDrawer()
+        fetchListContact()
         bindViews()
-        updateList()
+        //updateList()
+    }
+
+    private fun fetchListContact() {
+        val list = arrayListOf(
+            Contact(
+                "Vinícius Debur",
+                "(41) 99999-9999",
+                "img.png"
+            ),
+            Contact(
+                "Rafael Debur",
+                "(41) 99999-9999",
+                "img.png"
+            )
+        )
+        getInstanceSharedPreferences().edit {
+            putString("contacts", Gson().toJson(list))
+        }
+    }
+
+    private fun getInstanceSharedPreferences(): SharedPreferences {
+        return getSharedPreferences("com.example.listacontatos.PREFERENCES", Context.MODE_PRIVATE)
     }
 
     private fun initDrawer() {
@@ -40,7 +68,13 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
     }
@@ -48,23 +82,31 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private fun bindViews() {
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact> {
+        val list = getInstanceSharedPreferences().getString("contacts", "[]")
+        val turnsType = object : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
     }
 
     private fun updateList() {
-        adapter.updateList(
-            arrayListOf(
-                Contact(
-                    "Vinícius Debur",
-                    "(41) 99999-9999",
-                    "img.png"
-                ),
-                Contact(
-                    "Rafael Debur",
-                    "(41) 99999-9999",
-                    "img.png"
-                )
-            )
-        )
+        adapter.updateList(getListContacts())
+//        adapter.updateList(
+//            arrayListOf(
+//                Contact(
+//                    "Vinícius Debur",
+//                    "(41) 99999-9999",
+//                    "img.png"
+//                ),
+//                Contact(
+//                    "Rafael Debur",
+//                    "(41) 99999-9999",
+//                    "img.png"
+//                )
+//            )
+//        )
     }
 
     private fun showToast(message: String) {
